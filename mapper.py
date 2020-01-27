575a8b3e2347b47a3ac383dbc8510ce38f973712
laugaeddert <laugaeddert@gmail.com>
2020-01-27 20:27:05 +0000

    lowercase, remove stopwords, unsuccessful attempt to remove punctuation

--- mapper.py
+++ mapper.py
@@ -12,9 +12,17 @@ for line in sys.stdin:
     # split the line into words; splits on any whitespace
     words = line.split()
 
-    # output tuples (word, 1) in tab-delimited format
-    stopwords = set(['the', 'and'])
+    #remove punctuation
+    #punct  = set(['.',',',';',':','!','?'])
+    #no_punct = '' 
+    #for char in words:
+	#if char not in punct:
+	    # words = words + no_punct 
 
+    # output tuples (word, 1) in tab-delimited format - removing stopwords (stops)
+    from sklearn.feature_extraction import stop_words 
+    stops = set(stop_words.ENGLISH_STOP_WORDS)
+ 
     for word in words:
-     if word not in stopwords:
+     if word not in stops:
         print '%s\t%s' % (word, "1")
